1cf3cb930f69e5f672a51d4358c7b1910ef920ae
himali <admin>
2022-08-30 22:25:12 +0530

    done changes

--- src/main/java/com/example/droneapplication/repository/DroneRepository.java
+++ src/main/java/com/example/droneapplication/repository/DroneRepository.java
@@ -1,4 +1,8 @@
 package com.example.droneapplication.repository;
 
-public class DroneRepository {
+import com.example.droneapplication.model.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DroneRepository extends JpaRepository<Drone,Long> {
+
 }
